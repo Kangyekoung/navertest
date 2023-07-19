4c0a38110587d4f6756ce40baab68d8f12dea50a
kanghyekyoung <khg3694_@naver.com>
2023-07-19 11:40:34 +0900

    commit

---
+++ firstboot/src/main/java/board/spring/mybatis/BoardDAO.java
@@ -0,0 +1,35 @@
+package board.spring.mybatis;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+
+@Mapper
+@Repository
+public interface BoardDAO {
+	public int insertBoard(BoardDTO dto) ;
+	
+	public int getTotalBoard();
+
+	public List<BoardDTO> boardList(int[] limit);
+	
+	public int updateViewcount(int seq) ;
+	
+	public BoardDTO getDetail(int seq) ;
+	
+	public int deleteBoard(int seq) ;
+	
+	public int updateBoard(BoardDTO dto) ;
+	
+	public List<BoardDTO> searchList(HashMap map);
+	
+	public int getSearchBoard(HashMap map);
+	
+	public BoardMemberDTO boardWriterInform(int seq);
+	
+}
